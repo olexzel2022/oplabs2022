799faa16741ebcf34cf93b97cf152e65a206ef0c
Oleksandr Zelenskyi <olzel@smart-it.com>
2023-01-25 22:44:04 +0200

    added lab 5

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,32 @@
+﻿var x = new ConsoleWebServer() as IWebServer;
+x.Populate();
+x.ShowInfo();
+
+public class ConsoleWebServer : IWebServer
+{
+    string Name { get; set; }
+    string Url { get; set; }
+    int Port { get; set; }
+
+    public void Populate()
+    {
+        Console.WriteLine("Enter name:");
+        Name = Console.ReadLine();
+        Console.WriteLine("Enter url:");
+        Url = Console.ReadLine();
+        Console.WriteLine("Enter port:");
+        var x = int.TryParse(Console.ReadLine(), out var port);
+        Port = port;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Server: {Name}, url: {Url}, port: {Port}");
+    }
+}
+
+public interface IWebServer
+{
+    void Populate();
+    void ShowInfo();
+}
